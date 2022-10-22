b8233f12261f0db8684e0544a80b6d38ce2c25bd
MinhajSadik <MinhajoSadik@gmail.com>
2022-10-22 15:57:13 +0600

    [ADDED] typescript with react integrated for better and advanced learning

---
+++ src/Components/Lists.tsx
@@ -0,0 +1,13 @@
+import List from "./List";
+const Lists = () => {
+  const items: string[] = ["Minhaj", "Ahmed", "Sadik"];
+
+  const onClick = (text: string): void => alert(text);
+  return (
+    <div>
+      <List items={items} onClick={onClick} />
+    </div>
+  );
+};
+
+export default Lists;
